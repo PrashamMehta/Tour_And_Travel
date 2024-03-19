1ca37850a74ae5ce8282190a888bc5dd56ac4e47
Prasham Mehta <prasham.m1@ahduni.edu.in>
2024-03-19 14:44:12 +0530

    hotel page change

--- client/src/pages/hotel/Hotel.jsx
+++ client/src/pages/hotel/Hotel.jsx
@@ -11,10 +11,20 @@ import {
   faLocationDot,
 } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
+import {useLocation } from "react-router-dom";
+import useFetch from "../../hooks/useFetch.js";
 
 const Hotel = () => {
+  const location = useLocation();
+  
+  const id = location.pathname.split("/")[2];
+  
   const [slideNumber, setSlideNumber] = useState(0);
   const [open, setOpen] = useState(false);
+
+  const  { data, loading} = useFetch(`/hotels/find/${id}`)
+  
+
 
   const photos = [
     {
@@ -58,6 +68,8 @@ const Hotel = () => {
     <div>
       <Navbar />
       <Header type="list" />
+      {loading ? ("loading"
+      ) : (
       <div className="hotelContainer">
         {open && (
           <div className="slider">
@@ -83,13 +95,13 @@ const Hotel = () => {
         )}
         <div className="hotelWrapper">
           <button className="bookNow">Reserve or Book Now!</button>
-          <h1 className="hotelTitle">Tower Street Apartments</h1>
+          <h1 className="hotelTitle">{data.name}</h1>
           <div className="hotelAddress">
             <FontAwesomeIcon icon={faLocationDot} />
-            <span>Elton St 125 New york</span>
+            <span>{data.address}</span>
           </div>
           <span className="hotelDistance">
-            Excellent location – 500m from center
+            Excellent location – {data.distance}m from center
           </span>
           <span className="hotelPriceHighlight">
             Book a stay over $114 at this property and get a free airport taxi
@@ -108,19 +120,9 @@ const Hotel = () => {
           </div>
           <div className="hotelDetails">
             <div className="hotelDetailsTexts">
-              <h1 className="hotelTitle">Stay in the heart of City</h1>
+              <h1 className="hotelTitle">{data.title}</h1>
               <p className="hotelDesc">
-                Located a 5-minute walk from St. Florian's Gate in Krakow, Tower
-                Street Apartments has accommodations with air conditioning and
-                free WiFi. The units come with hardwood floors and feature a
-                fully equipped kitchenette with a microwave, a flat-screen TV,
-                and a private bathroom with shower and a hairdryer. A fridge is
-                also offered, as well as an electric tea pot and a coffee
-                machine. Popular points of interest near the apartment include
-                Cloth Hall, Main Market Square and Town Hall Tower. The nearest
-                airport is John Paul II International Kraków–Balice, 16.1 km
-                from Tower Street Apartments, and the property offers a paid
-                airport shuttle service.
+                {data.desc}
               </p>
             </div>
             <div className="hotelDetailsPrice">
@@ -138,7 +140,7 @@ const Hotel = () => {
         </div>
         <MailList />
         <Footer />
-      </div>
+      </div>)}
     </div>
   );
 };
